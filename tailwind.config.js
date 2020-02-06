9baba5afea6d57ab77c781e343d2366c8da9d0c9
AndreBClark <andre@cosmicdivision.dev>
2020-02-05 22:30:52 -0800

    :recycle: change values to string

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,6 @@ const darken = require('polished/lib/color/darken');
 module.exports = {
     theme: {
         extend: {
-            fontFamily: {
-                headingone: "Poppins",
-                headingtwo: "Montserrat"
-            },
             spacing: {
                 '72': '18rem',
                 '84': '21rem',
@@ -16,15 +12,15 @@ module.exports = {
             },
             colors: {
                 deeppurple: {
-                    900: darken(.25, "#44337a"),
-                    800: darken(.225, "#44337a"),
-                    700: darken(.2, "#44337a"),
-                    600: darken(.175, "#44337a"),
-                    500: darken(.15, "#44337a"),
-                    400: darken(.125, "#44337a"),
-                    300: darken(.1, "#44337a"),
-                    200: darken(.05, "#44337a"),
-                    100: darken(.025, "#44337a"),
+                    '900': darken(.25, "#44337a"),
+                    '800': darken(.225, "#44337a"),
+                    '700': darken(.2, "#44337a"),
+                    '600': darken(.175, "#44337a"),
+                    '500': darken(.15, "#44337a"),
+                    '400': darken(.125, "#44337a"),
+                    '300': darken(.1, "#44337a"),
+                    '200': darken(.05, "#44337a"),
+                    '100': darken(.025, "#44337a"),
                 }
             }
         }
