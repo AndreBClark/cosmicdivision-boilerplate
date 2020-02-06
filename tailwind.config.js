b971383b3f70c5f2a301f5fb28485d1f79247f87
AndreBClark <andre@cosmicdivision.dev>
2020-02-05 22:26:39 -0800

    :sparkles: add tailwind theme

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+const tint = require('polished/lib/color/tint');
+const darken = require('polished/lib/color/darken');
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                headingone: "Poppins",
+                headingtwo: "Montserrat"
+            },
+            spacing: {
+                '72': '18rem',
+                '84': '21rem',
+                '96': '24rem',
+                '108': '27rem',
+                '120': '30rem'
+            },
+            colors: {
+                deeppurple: {
+                    900: darken(.25, "#44337a"),
+                    800: darken(.225, "#44337a"),
+                    700: darken(.2, "#44337a"),
+                    600: darken(.175, "#44337a"),
+                    500: darken(.15, "#44337a"),
+                    400: darken(.125, "#44337a"),
+                    300: darken(.1, "#44337a"),
+                    200: darken(.05, "#44337a"),
+                    100: darken(.025, "#44337a"),
+                }
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+}
